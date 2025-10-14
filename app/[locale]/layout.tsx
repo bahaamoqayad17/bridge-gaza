8c59ec7218b434364db6cd3d4b908aecdec6acc2
bahaamoqayad17 <bahaa200030.m7taref@gmail.com>
2025-10-14 15:36:26 +0200

    setup the next.js application for multi language website with next-intl

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { routing } from "@/locales/routing";
+import { setRequestLocale } from "next-intl/server";
+
+export const metadata: Metadata = {
+  title: "Bridge Gaza",
+  description:
+    "Connecting Gaza to the world through mentorship and collaboration.",
+};
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+
+  setRequestLocale(locale);
+
+  if (!hasLocale(routing.locales, locale)) {
+    return <>not found page</>;
+  }
+  return (
+    <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+      <body className={`antialiased`}>
+        <NextIntlClientProvider locale={locale}>
+          <Navbar />
+          {children}
+          <Footer />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
